bfa035bca003f1099dc76e737f702b4ea07aaede
Aaron <aaronhdost@gmail.com>
2021-04-04 01:47:23 -0400

    RNG function editing

--- CSCN71030W21-TextAdventureGame/random.c
+++ CSCN71030W21-TextAdventureGame/random.c
@@ -1,5 +1,17 @@
-#include <time.h>
-#include <stdlib.h>
+#include <stdio.h>
 
-srand(time(NULL));   // Initialization, should only be called once.
-int r = rand();
+int RNG()
+{
+	int r, a, b;
+	puts("100 Random Numbers");
+	for (a = 0; a < 20; a++)
+	{
+		for (b = 0; b < 5; b++)
+		{
+			r = rand();
+			printf("%dt", r);
+		}
+		putchar('n');
+	}
+	return(0);
+}
