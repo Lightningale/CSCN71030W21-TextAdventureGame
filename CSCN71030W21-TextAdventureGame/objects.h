9d8b2cf3cc06fc83f8a55094ff4817ed8275c62c
Aaron <aaronhdost@gmail.com>
2021-04-10 15:00:13 -0400

    added descriptions for object header

--- CSCN71030W21-TextAdventureGame/objects.h
+++ CSCN71030W21-TextAdventureGame/objects.h
@@ -4,13 +4,13 @@
 #include <string.h>
 
 struct object {
-	const char* description;
-	const char* tag;
-	struct object* location;
+	const char* description; // description of object
+	const char* tag; // object tag, short form name used for calling object
+	struct object* location; // location of object
 }
 objs[] = {
-	{"test location 1", "loc1", NULL},
+	{"test location 1", "loc1", NULL}, //merged location and object list into one, so these first 2 objects are the locations where players can go
 	{"test location 2", "loc2", NULL},
-	{"test coin", "gold", &objs[0]},
+	{"test coin", "gold", &objs[0]}, // a test object, gold coin, location is in "test location 1"
 	{"test weapon"  , "sword"  , &objs[1]},
 };
