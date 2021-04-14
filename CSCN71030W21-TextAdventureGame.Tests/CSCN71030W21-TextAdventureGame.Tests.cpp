7045459f28986600f60325e24412441336b4335a
Lightningale <gyl.lakewood@gmail.com>
2021-04-14 16:35:47 -0400

    Created unit test project

---
+++ CSCN71030W21-TextAdventureGame.Tests/CSCN71030W21-TextAdventureGame.Tests.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+extern "C" {
+#include "functions.h"
+}
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CSCN71030W21TextAdventureGameTests
+{
+	TEST_CLASS(CSCN71030W21TextAdventureGameTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
